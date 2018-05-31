6e2f767fb36edf4e9b692f33dcbd97b68e441d4c
johnsonyue <johnsonuuehit@163.com>
2018-05-31 01:48:35 -0400

    index

--- import/bulk.sql
+++ import/bulk.sql
@@ -16,8 +16,13 @@ create table if not exists edge_table(
   in_country varchar(2),
   out_country varchar(2),
   primary key (in_ip, out_ip)
-);
+) engine MyISAM;
 
 load data local infile 'data.tsv'
 into table edge_table
 fields terminated by ' ';
+
+create index in_ip_index on edge_table(in_ip);
+create index out_ip_index on edge_table(out_ip);
+create index in_country_index on edge_table(in_country);
+create index out_country_index on edge_table(out_country);
